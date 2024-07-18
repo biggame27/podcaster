75e9dd714f85dec9e6b657b38a00bdc83ec8a398
biggame27 <koharrisonko@gmail.com>
2024-07-18 14:49:36 -0500

    first commit

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,64 @@
+'use client';
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import Header from './Header';
+import Carousel from './Carousel';
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import LoaderSpinner from './LoaderSpinner';
+
+const RightSidebar = () => {
+  const { user } = useUser();
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount)
+  const router = useRouter();
+
+  if (!topPodcasters) return <LoaderSpinner />
+  return (
+    <section className="right_sidebar text-white-1">
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-4 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">{user?.firstName} {user?.lastName}</h1>
+            <Image 
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      </SignedIn>
+
+      <section>
+        <Header headerTitle="Fans Like You" />
+        <Carousel fansLikeDetail={topPodcasters!} />
+      </section>
+
+      <section className="flex flex-col gap-8 pt-12">
+        <Header headerTitle="Top Podcasters" />
+        <div className="flex flex-col gap-6">
+          {topPodcasters?.slice(0,4).map((podcaster) => (
+            <div key={podcaster._id} className="flex cursor-pointer justify-between" onClick={() => router.push(`/profile/${podcaster.clerkId}`)}>
+              <figure className="flex items-center gap-2">
+                <Image 
+                  src={podcaster.imageUrl}
+                  alt={podcaster.name}
+                  width={44}
+                  height={44}
+                  className="aspect-square rounded-lg"
+                />
+                <h2 className="text-14 font-semibold text-white-1">{podcaster.name}</h2>
+              </figure>
+            </div>
+          ))}
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default RightSidebar
---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://bright-garfish-3.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://bright-garfish-3.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
